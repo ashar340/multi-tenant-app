d69ba59bb911b87a44f7a34c5433455d1c49f1df
Ananya <ananyas237@gmail.com>
2020-08-27 10:40:50 -0400

    interceptor created

---
+++ src/app/tenant/tenant.interceptor.ts
@@ -0,0 +1,19 @@
+import { Injectable } from "@angular/core";
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { TenantService } from "./tenant.service";
+
+@Injectable()
+export  class TenantInterceptor implements HttpInterceptor {
+  constructor(private tenantService: TenantService) {}
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const headers = this.tenantService.addTenantToHeaders(req.headers);
+
+    req = req.clone({
+      headers: headers
+    });
+
+    return next.handle(req);
+  }
+}
--- src/app/tenant/tenant.service.ts
+++ src/app/tenant/tenant.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpHeaders } from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,34 @@ import { Injectable } from '@angular/core';
 export class TenantService {
 
   constructor() { }
+
+  getTenantForHostName(hostname: string): Tenant {
+    return this.getTenantForHostName(hostname.split(".")[0]);
+  }
+
+  getTenantForString(s: string) {
+    for (const e in Tenant) {
+      if (e.toLowerCase() === s.toLowerCase()) {
+        return Tenant[e] as Tenant;
+      }
+    }
+    return null;
+  }
+
+  getTenantForHost(host: string): Tenant {
+    return this.getTenantForString(host);
+  }
+
+  getTenant(): Tenant {
+    return this.getTenantForHostName(location.hostname);
+  }
+
+  addTenantToHeaders(headers: HttpHeaders): HttpHeaders {
+    return headers.append("X-Tenant-ID", this.getTenant());
+  }
+}
+
+export enum Tenant {
+  CLIENT1 = "client1",
+  CLIENT2 = "client2"
 }
